7b2470d837b61fcc89202f5dc98d4adaa9b55acf
Mario Döring <deluna.yy@gmail.com>
2022-07-19 00:14:52 +0200

    testing buffer error on CI

--- examples/buffer.php
+++ examples/buffer.php
@@ -2,17 +2,17 @@
 
 $buffer = new GL\Buffer\IntBuffer();
 
-// $bufer->reserve(5);
-$buffer->fill(10, 42);
-$buffer[1] = 12123123;
-$buffer[6] = 77565756;
+// // $bufer->reserve(5);
+// $buffer->fill(10, 42);
+// $buffer[1] = 12123123;
+// $buffer[6] = 77565756;
 
-foreach($buffer as $k => $v) {
-    var_dump($k, $v);
-}
+// foreach($buffer as $k => $v) {
+//     var_dump($k, $v);
+// }
 
-echo "---";
+// echo "---";
 
-foreach($buffer as $k => $v) {
-    var_dump($k, $v);
-}
+// foreach($buffer as $k => $v) {
+//     var_dump($k, $v);
+// }
