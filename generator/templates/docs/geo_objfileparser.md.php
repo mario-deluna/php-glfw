02516fdf05164aa052a016c42f94e572991b9421
Mario Döring <deluna.yy@gmail.com>
2022-09-20 22:58:44 +0200

    Usage on ObjFIleParser

--- generator/templates/docs/geo_objfileparser.md.php
+++ generator/templates/docs/geo_objfileparser.md.php
@@ -15,6 +15,58 @@ namespace GL\Geometry
 
 ## Usage 
 
+### Loading a .obj file 
+
+```php
+use GL\Geometry\ObjFileParser;
+
+$model = new \GL\Geometry\ObjFileParser('path/to/your/3dmodel.obj');
+```
+
+### Extracting all vertices 
+
+If you don't care about materials and just want to extract all vertices from the .obj file, you can using the `getVertices` method.
+
+```php
+$vertices = $model->getVertices('p');
+```
+
+Noticed the `p` argument? This is the vertex attribute you want to extract. 
+In this example we only want to extract the position data, which is what `p` stands for.
+
+Check out the [getVertices](#getvertices) method for more information about the available vertex attributes.
+
+`$vertices` is now a [FloatBuffer](/API/Buffer/FloatBuffer.html) object that contains the positions of all vertices in the .obj file.
+
+```php
+var_dump($vertices); // [
+    1.0, 2.0, 3.0, // vertex 1
+    4.0, 5.0, 6.0, // vertex 2
+    7.0, 8.0, 9.0, // vertex 3
+    // ...
+// ]
+```
+
+### Extracting Meshes
+
+If your model is made up of multiple materials and you want to extract the vertices for each material, you can use the `getMeshes` method.
+This method is going to be slower than the `getVertices` method, because it has to group the vertices by material. But it's still pretty fast,
+and for sure faster then if you would implement the grouping in PHP.
+
+One main difference between the `getVertices` and `getMeshes` method is that the `getMeshes` method returns an array of [Mesh](/API/Geometry/Mesh.html) objects.
+
+```php
+// extract `pnc` [position, normal, texture coords]
+$meshes = $model->getMeshes('pnc');
+
+foreach ($meshes as $mesh) {
+    // do something with the mesh
+}
+```
+
+!!! tip "Example"
+
+    A simple example to extract the diffuse color of a typical low poly model would be can be seen [here](https://github.com/mario-deluna/php-glfw/blob/master/examples/05_objloading.php#L60)
 
 ## Properties
 
