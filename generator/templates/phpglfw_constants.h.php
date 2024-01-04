13f21ba32099b78c233d17142821a8dd35ac35a6
Mario Döring <deluna.yy@gmail.com>
2024-01-04 12:10:24 +0100

    Yearly date update

--- generator/templates/phpglfw_constants.h.php
+++ generator/templates/phpglfw_constants.h.php
@@ -3,7 +3,7 @@
  * 
  * Extension constants
  *
- * Copyright (c) 2018-2023 Mario Döring
+ * Copyright (c) 2018-2024 Mario Döring
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- phpglfw_vg.h
+++ phpglfw_vg.h
@@ -3,7 +3,7 @@
  * 
  * Vector Graphics (powered by NanoVG)
  *
- * Copyright (c) 2018-2023 Mario Döring
+ * Copyright (c) 2018-2024 Mario Döring
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
